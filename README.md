a965e444005f1cde0e01b62cf41551dbe0c1a2d2
MANOJ NAIDU MAMILLAPALLI <113248057+manojmamillapalli@users.noreply.github.com>
2023-03-12 01:10:26 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+#'yarn install'
+use the above command on the client side to install dependencies.
+#'npm install;
+use the above command on the server side to install  dependencies
+#'yarn start'
+Use the above command on the client side to run the client
+#'npm start'
+use the above command on the server side to run server side
---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import userRoutes from './routes/users.js'
+import questionRoutes from './routes/Questions.js'
+import dotenv from "dotenv";
+import answerRoutes from'./routes/Answers.js'
+dotenv.config(); 
+const app=express();
+app.use(express.json({limit: "30mb" , extended:true}))
+app.use(express.urlencoded({limit:"30mb", extended:true }))
+app.use(cors());
+app.get('/', (req,res)=>{
+res.send("This is a stack overflow clone API")
+})
+app.use("/user",userRoutes)
+app.use("/questions",questionRoutes)
+app.use('/answer',answerRoutes)
+const PORT =process.env.PORT || 5000
+
+const DATABASE_URL=process.env.CONNECTION_URL
+mongoose.set('strictQuery', false);
+mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=> app.listen(PORT, () => { console.log('server running on port $(PORT 5000)')}))
+.catch((err)=> console.log(err.message))
